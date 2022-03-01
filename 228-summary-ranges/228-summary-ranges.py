4753e1d52d8ff1f292eebcc95735a851d1f13e72
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-03-01 22:07:04 +0530

    Time: 47 ms (39.02%), Space: 14 MB (60.03%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -1,20 +1,15 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         res = []
-        
-        for num in nums:
-            if not res or num != res[-1][-1]+1:
+        for i in nums:
+            if not res or i != res[-1][-1] + 1:
                 if res and len(res[-1]) >= 2:
                     res[-1] = [res[-1][0]] + [res[-1][-1]]
-                res.append([num])
-            
+                res.append([i])
             else:
-                res[-1].append(num)
-                
+                res[-1].append(i)
         if res and len(res[-1]) >= 2:
             res[-1] = [res[-1][0]] + [res[-1][-1]]
-        
-        return ['->'.join(map(str,x))for x in res]
-       
-       
+                
+        return ['->'.join(map(str,x)) for x in res]
         
