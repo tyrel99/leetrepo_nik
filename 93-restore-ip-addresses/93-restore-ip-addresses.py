0439b207bf004489871957b7a303adb97aba11aa
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-03-13 21:56:24 +0530

    Time: 45 ms (70.68%), Space: 14.1 MB (32.34%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -1,22 +1,26 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        #check len of s
         res = []
-        
         if len(s) > 12:
-            return res 
+            return res
         
-        def Backtrack(i,dots,curIP):
+        def Backtrack(i, dots, currIP):
+            #1st basecase (check if dots == 4 and we reach len of nums)
             if dots == 4 and i == len(s):
-                res.append(curIP[:-1])
-                return
+                res.append(currIP[:-1])
             
+            #2nd basecase (check dots exceed more than 4)
             if dots > 4:
                 return
             
-            for j in range(i,min(i+3,len(s))):
-                if int(s[i:j+1]) < 256 and (i == j or s[i] != '0'):
-                    Backtrack(j+1, dots+1, curIP + s[i:j+1] + '.')
-        Backtrack(0,0,'')
+            for j in range(i, min(i+3, len(s))): # Loop from i to min(of given condition)
+                if int(s[i:j+1]) < 256 and (i == j or s[i] != '0'): # check if digit is less than equal to 255 and leading 0's are not present
+                    
+                    Backtrack(j+1, dots + 1, currIP + s[i:j+1] + '.')
+            
+        Backtrack(0,0,"")
         return res
                 
+        
         
