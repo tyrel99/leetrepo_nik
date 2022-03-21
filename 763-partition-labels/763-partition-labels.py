659b346422383f0710f925e48afed83eb716de0d
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-03-21 15:31:51 +0530

    Time: 58 ms (52.46%), Space: 13.9 MB (78.97%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {s[i]: i for i in range(len(s))}
+        
+        i,ans = 0, []
+        
+        while i < len(s):
+            end, j = last[s[i]], i + 1
+            while j < end:
+                if last[s[j]] > end:
+                    end = last[s[j]]
+                j += 1
+            ans.append(end - i + 1)
+            i = end + 1
+        return ans
+       
+                
+                
+        
