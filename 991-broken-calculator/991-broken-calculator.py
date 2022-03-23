0c32e5b3e01a5b6058ce3cbd9a2ad059630dc6fd
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-03-23 22:09:12 +0530

    Time: 62 ms (8.68%), Space: 13.9 MB (25.11%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.py
+++ 991-broken-calculator/991-broken-calculator.py
@@ -1,12 +1,9 @@
 class Solution:
     def brokenCalc(self, startValue: int, target: int) -> int:
-        count = 0
-        
-        while target > startValue:
-            if target % 2 == 0:
-                target //= 2
-            else:
-                target += 1
-            count += 1
-        return count + startValue - target
-        
+        if target <= startValue:
+            return startValue - target
+        if target % 2 == 0:
+            return 1 + self.brokenCalc(startValue,target // 2)
+        else:
+            return 1 + self.brokenCalc(startValue,target + 1)
+       
