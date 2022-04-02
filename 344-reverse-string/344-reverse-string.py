03367f50cc74b62520d946172727123ca980fea6
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-04-02 10:16:29 +0530

    Time: 392 ms (10.22%), Space: 42.6 MB (7.56%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,6 +1,15 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s[:] = s[::-1]
+        #USING RECURSSION
+        N = len(s)
+        def swap(i):
+            #base condition
+            if i >= N//2:
+                return
+            s[i], s[N-i-1] = s[N-i-1], s[i]
+            swap(i+1)
+        swap(0)
+        
         """
         Do not return anything, modify s in-place instead.
         """
