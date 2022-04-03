cad958c226497e6d6f1e6686454ff905197e8cf0
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-04-03 21:01:06 +0530

    Time: 58 ms (54.17%), Space: 13.8 MB (98.64%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        length = len(nums)
+        
+        #base case
+        if length <= 2:
+            return nums.reverse()
+        
+        pointer = length - 2
+        
+        #for descending elements
+        while pointer >= 0 and nums[pointer] >= nums[pointer + 1]:
+            pointer -= 1
+        if pointer == -1:
+            return nums.reverse()
+        
+        for x in range(length - 1,pointer,-1):
+            if nums[pointer] < nums[x]:
+                nums[pointer],nums[x] = nums[x],nums[pointer]
+                break
+                
+        nums[pointer + 1:] = reversed(nums[pointer+1:]) 
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
