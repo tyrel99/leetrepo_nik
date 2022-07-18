a272b4f53e8b321fdd4ae082ecd7a53cdf8ee1c8
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-07-18 14:50:32 +0530

    Time: 604 ms (22.50%), Space: 21.9 MB (19.34%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,23 +3,40 @@ class Solution:
         if not grid:
             return 0
         
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid,i,j)
-                    count += 1
+        rows,cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+        
+        def bfs(r,c):
+            queue = collections.deque()
+            visit.add((r,c))
+            queue.append((r,c))
+        
+            while queue:
+                row,col = queue.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+            
+                for dr,dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r in range(rows) and
+                        c in range(cols) and 
+                        grid[r][c] == '1' and 
+                        (r,c) not in visit):
                     
-        return count
+                        queue.append((r,c))
+                        visit.add((r,c))
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visit:
+                    bfs(r,c)
+                    islands += 1
+        return islands
     
-    def dfs(self,grid,i,j):
-        if (i<0 or j<0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1'):
-            return
+    
+    
+                
+            
         
-        grid[i][j] = '#'
         
-        self.dfs(grid,i+1,j)
-        self.dfs(grid,i-1,j)
-        self.dfs(grid,i,j+1)
-        self.dfs(grid,i,j-1)
         
