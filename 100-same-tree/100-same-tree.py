7a0103b2230615dab006692213c872c656f438d6
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-08-20 13:02:49 +0530

    Time: 55 ms (29.39%), Space: 13.9 MB (29.86%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,22 +6,7 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        #Bfs Traversal
-        queue = [(p,q)]
-        
-        while queue:
-            nd1,nd2 = queue.pop(0)
-            
-            if not nd1 and not nd2:
-                continue
-            elif None in (nd1,nd2):
-                return False
-            else:
-                if nd1.val != nd2.val:
-                    return False
-                queue.append((nd1.left,nd2.left))
-                queue.append((nd1.right,nd2.right))
-                
-        return True
-        
-        
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return p == q
