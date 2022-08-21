d3c37f00908733bc2dc0ad667643019e94b68458
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-08-21 11:22:14 +0530

    Time: 1326 ms (100.00%), Space: 15.5 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,9 +1,24 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
-        
-        count = Counter(num)
-        print(count)
-        s = ''.join(count[i] // 2 * i for i in "9876543210").lstrip("0")
-        mid  = max(count[i] % 2 * i for i in count)
-        return s + mid + s[::-1] or "0"
-     
+        c = Counter(num)
+        if len(c)==1 and c['0']>=1:
+            return "0"
+        m = -1   #storing the mid of the number
+        res1 = ''
+        res2 = ''
+        for i in range(9,-1,-1):
+            while c[str(i)]:
+                if not res1 and i==0:   #to avoid adding 0 at first
+                    break 
+                if c[str(i)]>=2:
+
+                    res1 += str(i)
+                    res2 = str(i) + res2
+                    c[str(i)]-=2
+                if c[str(i)] == 1:
+                    m = max(m,i)  #updating medium to max
+                    c[str(i)]-=1
+                if c[str(i)]==0:
+                    del c[str(i)]
+        return res1+res2 if m==-1 else res1 + str(m) + res2
+       
