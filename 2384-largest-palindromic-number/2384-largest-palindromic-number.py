55e3615e7a076ceb8ee1bf1e13de2ea49f77bcc9
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-08-21 12:12:09 +0530

    Time: 1289 ms (100.00%), Space: 15.5 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,24 +1,31 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
-        c = Counter(num)
-        if len(c)==1 and c['0']>=1:
-            return "0"
-        m = -1   #storing the mid of the number
+        
+        count = Counter(num)
+        
+        if len(count) == 1 and count['0'] >= 1:
+            return '0'
+        
         res1 = ''
         res2 = ''
+        mid = -1
+        
         for i in range(9,-1,-1):
-            while c[str(i)]:
-                if not res1 and i==0:   #to avoid adding 0 at first
-                    break 
-                if c[str(i)]>=2:
-
+            while count[str(i)]:
+                if not res1 and i == 0:
+                    break
+                if count[str(i)] >= 2:
                     res1 += str(i)
                     res2 = str(i) + res2
-                    c[str(i)]-=2
-                if c[str(i)] == 1:
-                    m = max(m,i)  #updating medium to max
-                    c[str(i)]-=1
-                if c[str(i)]==0:
-                    del c[str(i)]
-        return res1+res2 if m==-1 else res1 + str(m) + res2
-       
+                    count[str(i)] -= 2
+                if count[str(i)] == 1:
+                    mid = max(mid,i)
+                    count[str(i)] -= 1
+                if count[str(i)] == 0:
+                    del count[str(i)]
+                
+        return res1 + res2 if mid == -1 else res1 + str(mid) + res2
+                    
+                    
+        
+        
