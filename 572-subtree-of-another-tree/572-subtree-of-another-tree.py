fe62d6fd45957ff2ef754ba421943fd3ccd01c7c
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2022-08-23 19:25:06 +0530

    Time: 166 ms (65.93%), Space: 15.2 MB (35.97%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -11,48 +11,15 @@ class Solution:
         if root == None:
             return False
         
-        sameTree = self.isSametree(root,subRoot)
-        subleft = self.isSubtree(root.left,subRoot)
-        subright = self.isSubtree(root.right,subRoot)
-        
-        return subleft or subright or sameTree
+        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot) or self.sameTree(root,subRoot)
         
         
         
-    def isSametree(self,root,subRoot):
+    def sameTree(self,root,subRoot):
         if root == None and subRoot == None:
             return True
         if root == None or subRoot == None:
             return False
-        if root.val != subRoot.val:
-            return False
-        return self.isSametree(root.left,subRoot.left) and self.isSametree(root.right,subRoot.right)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        """  
-        p = root
-        q = subroot
-        queue = [(p,q)]
-        
-        while queue:
-            node1,node2 = queue.pop(0)
-            if None in (node1,node2):
-                return False
-            if not node1 and not node2:
-                continue
-            else:
-                if node1.val != node2.val:
-                    queue.append((node1.left,node2))
-                    queue.append((node2.right,node2))
-        """
-                
-                    
-        
+        return root.val == subRoot.val and self.sameTree(root.left,subRoot.left) and self.sameTree(root.right,subRoot.right)
         
