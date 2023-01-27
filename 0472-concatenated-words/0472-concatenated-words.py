0926a7f931692c2373f234372eb314ee026a3d72
tyrel99 <82277614+tyrel99@users.noreply.github.com>
2023-01-27 19:30:23 +0530

    Time: 434 ms (64.67%), Space: 27.8 MB (37.94%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -2,14 +2,18 @@ class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         sett = set(words)
         res = []
+        dp = []
         mini = 10000
         def checkword(word):
+            if word in dp:
+                return True
             for i in range(mini, len(word)):
                 lf = word[:i]
                 rf = word[i:]
                 
                 if lf in sett:
                     if rf in sett or checkword(rf):
+                        dp.append(word)
                         return True
         
         for i in words:
